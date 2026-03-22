e6b0db1a770b5aaa595def0aaa855296e6e121e4
o-kyoko <k.k.k@lime.ocn.ne.jp>
2026-03-22 14:09:53 +0900

    swiper 7

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,24 @@
 //  swiper js //
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
-      loop: true,
-      speed: 2000,
-      autoplay: {
-      delay: 3000, // スライドが切り替わるまでの表示時間(ミリ秒)
-      stopOnLast: false, // 最後のスライドまで表示されたら自動再生を中止するか
-      disableOnInteraction: true // ユーザーのスワイプ操作を検出したら自動再生を中止するか
-     },
-      pagination: {
-        el: '.swiper-pagination', // ページネーションの要素
-        clickable: true, // クリック可能にする
-      },
-      navigation: {
-        nextEl: '.swiper-button-next', // 次へボタン
-        prevEl: '.swiper-button-prev', // 前へボタン
-      }
-      
+loop: true,
+  speed:2000,//スライドが切り替わるのにかかる時間msec
+  autoplay:{
+  delay: 2000//次のスライドに切り替わるまでの時間msec
+  },
+  pagination: {
+    el: '.swiper-pagination',
+ },
+
+  //(3)ナビゲーションボタンを表示する時追加
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //(4)スクロールバーを表示する時追加
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  }
     });
   });
