b548362e31171c09c2cae19167e326121a9d8128
o-kyoko <k.k.k@lime.ocn.ne.jp>
2026-03-22 14:48:41 +0900

    swiper 12

--- js/swiper.js
+++ js/swiper.js
@@ -1,27 +1,21 @@
 //  swiper js //
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
+ const myswiper = new Swiper('.swiper', {
    loop: true,
-   centeredSliders: true,
-   speed: 3000,
-   autoplay: {
-    delay: 3000,   
-    disableOnInteraction: false, // デフォルト: true
-    pauseOnMouseEnter: false,    // デフォルト: true
-    waitForTransition: true,     // デフォルト: true
-    stopOnLastSlide: false,      // デフォルト: false
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    type: '.bullet',
+   centeredSlides : true,
+   autoplay: { 
+   delay: 3000,
+   disableOnInteraction: false,
  },
-
-  //(3)ナビゲーションボタンを表示する時追加
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+ pagination: { 
+   el: '.swiper-pagination',
+   clickable: true,
+   type: 'bullets',
+ }, 
+ navigation: {
+   nextEl:'.swiper-button-next',
+   prevEl:'.swiper-button-prev',
+ },
 
   //(4)スクロールバーを表示する時追加
   scrollbar: {
